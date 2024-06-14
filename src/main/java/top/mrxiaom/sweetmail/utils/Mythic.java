aa36384d781d6fef9234e4c92a93ee531a5e822d
MrXiaoM <coolxiaom95@gmail.com>
2024-06-15 00:01:49 +0800

    mythic mobs 4.x support

--- src/main/java/top/mrxiaom/sweetmail/utils/Mythic.java
+++ src/main/java/top/mrxiaom/sweetmail/utils/Mythic.java
@@ -1,6 +1,7 @@
 package top.mrxiaom.sweetmail.utils;
 
 import org.bukkit.inventory.ItemStack;
+import top.mrxiaom.sweetmail.utils.mythic.Mythic4;
 import top.mrxiaom.sweetmail.utils.mythic.Mythic5;
 
 import java.util.Optional;
@@ -15,7 +16,9 @@ public class Mythic {
         if (Util.isPresent("io.lumine.mythic.bukkit.MythicBukkit")) {
             proxy = new Mythic5();
         }
-        // TODO: Mythic4
+        if (Util.isPresent("io.lumine.xikage.mythicmobs.MythicMobs")) {
+            proxy = new Mythic4();
+        }
     }
 
     public static Optional<ItemStack> getItem(String name) {
---
+++ src/main/java/top/mrxiaom/sweetmail/utils/mythic/Mythic4.java
@@ -0,0 +1,16 @@
+package top.mrxiaom.sweetmail.utils.mythic;
+
+import io.lumine.xikage.mythicmobs.MythicMobs;
+import io.lumine.xikage.mythicmobs.adapters.bukkit.BukkitAdapter;
+import org.bukkit.inventory.ItemStack;
+import top.mrxiaom.sweetmail.utils.Mythic;
+
+import java.util.Optional;
+
+public class Mythic4 implements Mythic.IMythic {
+    MythicMobs inst = MythicMobs.inst();
+    @Override
+    public Optional<ItemStack> getItem(String name) {
+        return inst.getItemManager().getItem(name).map(it -> it.generateItemStack(1)).map(BukkitAdapter::adapt);
+    }
+}
