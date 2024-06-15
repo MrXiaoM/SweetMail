95f9fa3be76cf34b9303b8d28a007f33308cfe18
MrXiaoM <coolxiaom95@gmail.com>
2024-06-15 15:47:39 +0800

    abstract sql database

---
+++ src/main/java/top/mrxiaom/sweetmail/database/impl/AbstractSQLDatabase.java
@@ -0,0 +1,208 @@
+package top.mrxiaom.sweetmail.database.impl;
+
+import top.mrxiaom.sweetmail.SweetMail;
+import top.mrxiaom.sweetmail.database.IMailDatabase;
+import top.mrxiaom.sweetmail.database.entry.Mail;
+import top.mrxiaom.sweetmail.database.entry.MailWithStatus;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractSQLDatabase implements IMailDatabase {
+    protected String TABLE_BOX, TABLE_STATUS;
+    protected abstract Connection getConnection() throws SQLException;
+
+    protected void createTables() {
+        try {
+            Connection conn = getConnection();
+            PreparedStatement ps = conn.prepareStatement(
+                    "CREATE TABLE if NOT EXISTS `" + TABLE_BOX + "`(" +
+                            "`uuid` VARCHAR(32) PRIMARY KEY," +
+                            "`sender` VARCHAR(32)," +
+                            "`data` MEDIUMBLOB," +
+                            "`time` TIMESTAMP" +
+                    ");" +
+                    "CREATE TABLE if NOT EXISTS `" + TABLE_STATUS + "`(" +
+                            "`uuid` VARCHAR(32)," +
+                            "`receiver` VARCHAR(32)," +
+                            "`read` TINYINT(1) DEFAULT 0," +
+                            "`used` TINYINT(1) DEFAULT 0," +
+                            "PRIMARY KEY(`uuid`, `receiver`)" +
+                    ");");
+            ps.execute();
+        } catch (SQLException e) {
+            SweetMail.warn(e);
+        }
+    }
+
+    @Override
+    public void sendMail(Mail mail) {
+        try {
+            Connection conn = getConnection();
+            try (PreparedStatement ps = conn.prepareStatement("INSERT INTO `" + TABLE_BOX + "`(`uuid`,`sender`,`data`,`time`) VALUES(?, ?, ?, NOW());")) {
+                ps.setString(1, mail.uuid);
+                ps.setString(2, mail.sender);
+                byte[] bytes = mail.serialize().getBytes(StandardCharsets.UTF_8);
+                try (InputStream in = new ByteArrayInputStream(bytes)) {
+                    ps.setBinaryStream(3, in);
+                    ps.execute();
+                }
+            }
+            try (PreparedStatement ps = conn.prepareStatement("INSERT INTO `" + TABLE_STATUS + "`(`uuid`,`receiver`,`read`,`used`) VALUES(?, ?, 0, 0);")) {
+                for (String receiver : mail.receivers) {
+                    ps.setString(1, mail.uuid);
+                    ps.setString(2, receiver);
+                    ps.addBatch();
+                }
+                ps.executeBatch();
+            }
+            mail.noticeSent();
+        } catch (SQLException | IOException e) {
+            SweetMail.warn(e);
+        }
+    }
+
+    @Override
+    public List<MailWithStatus> getOutBox(String player, int page, int perPage) {
+        List<MailWithStatus> mailList = new ArrayList<>();
+        try {
+            int offset = (page - 1) * perPage;
+            Connection conn = getConnection();
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM " +
+                    "(`" + TABLE_STATUS + "` NATURAL JOIN (SELECT * FROM `" + TABLE_BOX + "` WHERE `sender` = ?) as A) " +
+                    "LIMIT " + offset + ", " + perPage + " " +
+                    "ORDER BY `time` DESC;");
+            ps.setString(1, player);
+            try (ResultSet result = ps.executeQuery()) {
+                while (result.next()) {
+                    mailList.add(resolveResult(result));
+                }
+            }
+        } catch (SQLException | IOException e) {
+            SweetMail.warn(e);
+        }
+        return mailList;
+    }
+
+    @Override
+    public List<MailWithStatus> getInBox(boolean unread, String player, int page, int perPage) {
+        List<MailWithStatus> mailList = new ArrayList<>();
+        try {
+            int offset = (page - 1) * perPage;
+            Connection conn = getConnection();
+            String conditions = unread
+                    ? "`receiver` = ? AND unread = 1"
+                    : "`receiver` = ?";
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM " +
+                    "(`" + TABLE_BOX + "` NATURAL JOIN (SELECT * FROM `" + TABLE_STATUS + "` WHERE " + conditions + ") as A) " +
+                    "ORDER BY `used` DESC, `time` DESC " +
+                    "LIMIT " + offset + ", " + perPage + ";");
+            ps.setString(1, player);
+            try (ResultSet result = ps.executeQuery()) {
+                while (result.next()) {
+                    mailList.add(resolveResult(result));
+                }
+            }
+        } catch (SQLException | IOException e) {
+            SweetMail.warn(e);
+        }
+        return mailList;
+    }
+
+    @Override
+    public List<MailWithStatus> getInBoxUnused(String player) {
+        List<MailWithStatus> mailList = new ArrayList<>();
+        try {
+            Connection conn = getConnection();
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM " +
+                    "(`" + TABLE_BOX + "` NATURAL JOIN (SELECT * FROM `" + TABLE_STATUS + "` WHERE `receiver` = ? AND `used` = 1) as A) " +
+                    "ORDER BY `time` DESC;");
+            ps.setString(1, player);
+            try (ResultSet result = ps.executeQuery()) {
+                while (result.next()) {
+                    mailList.add(resolveResult(result));
+                }
+            }
+        } catch (SQLException | IOException e) {
+            SweetMail.warn(e);
+        }
+        return mailList;
+    }
+
+    private MailWithStatus resolveResult(ResultSet result) throws IOException, SQLException {
+        String dataJson;
+        try (InputStream in = result.getBinaryStream("data")) {
+            try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+                byte[] buffer = new byte[1024 * 10];
+                int len;
+                while ((len = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, len);
+                }
+                dataJson = new String(out.toByteArray(), StandardCharsets.UTF_8);
+            }
+        }
+        LocalDateTime time = result.getTimestamp("time").toLocalDateTime();
+        boolean read = result.getInt("read") == 1;
+        boolean used = result.getInt("used") == 1;
+        return Mail.deserialize(dataJson, time, read, used);
+    }
+
+    @Override
+    public void markRead(String uuid, String receiver) {
+        try {
+            Connection conn = getConnection();
+            PreparedStatement ps = conn.prepareStatement("UPDATE `" + TABLE_STATUS + "` " +
+                    "SET `read` = 1" +
+                    "WHERE `uuid` = ? AND `receiver` = ?;");
+            ps.setString(1, uuid);
+            ps.setString(2, receiver);
+            ps.execute();
+        } catch (SQLException e) {
+            SweetMail.warn(e);
+        }
+    }
+
+    @Override
+    public void markAllRead(String receiver) {
+        try {
+            Connection conn = getConnection();
+            PreparedStatement ps = conn.prepareStatement("UPDATE `" + TABLE_STATUS + "` " +
+                    "SET `read` = 1" +
+                    "WHERE `receiver` = ?;");
+            ps.setString(1, receiver);
+            ps.execute();
+        } catch (SQLException e) {
+            SweetMail.warn(e);
+        }
+    }
+
+    @Override
+    public void markUsed(List<String> uuidList, String receiver) {
+        try {
+            Connection conn = getConnection();
+            try (PreparedStatement ps = conn.prepareStatement("UPDATE `" + TABLE_STATUS + "` " +
+                    "SET `used` = 1" +
+                    "WHERE `uuid` = ? AND `receiver` = ?;")) {
+                for (String uuid : uuidList) {
+                    ps.setString(1, uuid);
+                    ps.setString(2, receiver);
+                    ps.addBatch();
+                }
+                ps.executeBatch();
+            }
+        } catch (SQLException e) {
+            SweetMail.warn(e);
+        }
+    }
+
+}
--- src/main/java/top/mrxiaom/sweetmail/database/impl/MySQLDatabase.java
+++ src/main/java/top/mrxiaom/sweetmail/database/impl/MySQLDatabase.java
@@ -3,24 +3,12 @@ package top.mrxiaom.sweetmail.database.impl;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.bukkit.configuration.MemoryConfiguration;
-import top.mrxiaom.sweetmail.SweetMail;
-import top.mrxiaom.sweetmail.database.IMailDatabase;
-import top.mrxiaom.sweetmail.database.entry.Mail;
-import top.mrxiaom.sweetmail.database.entry.MailWithStatus;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.sql.*;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.SQLException;
 
-public class MySQLDatabase implements IMailDatabase {
+public class MySQLDatabase extends AbstractSQLDatabase {
     HikariDataSource dataSource = null;
-    String TABLE_BOX, TABLE_STATUS;
     public void reload(MemoryConfiguration config) {
         if (dataSource != null) dataSource.close();
         String type = config.getString("database.type", "file");
@@ -46,186 +34,12 @@ public class MySQLDatabase implements IMailDatabase {
         hikariConfig.setPassword(pass);
         dataSource = new HikariDataSource(hikariConfig);
 
-        try {
-            Connection conn = dataSource.getConnection();
-            PreparedStatement ps = conn.prepareStatement(
-                    "CREATE TABLE if NOT EXISTS `" + TABLE_BOX + "`(" +
-                            "`uuid` VARCHAR(32) PRIMARY KEY," +
-                            "`sender` VARCHAR(32)," +
-                            "`data` MEDIUMBLOB," +
-                            "`time` TIMESTAMP" +
-                    ");" +
-                    "CREATE TABLE if NOT EXISTS `" + TABLE_STATUS + "`(" +
-                            "`uuid` VARCHAR(32)," +
-                            "`receiver` VARCHAR(32)," +
-                            "`read` TINYINT(1) DEFAULT 0," +
-                            "`used` TINYINT(1) DEFAULT 0," +
-                            "PRIMARY KEY(`uuid`, `receiver`)" +
-                    ");");
-            ps.execute();
-        } catch (SQLException e) {
-            SweetMail.warn(e);
-        }
+        createTables();
     }
 
     @Override
-    public void sendMail(Mail mail) {
-        try {
-            Connection conn = dataSource.getConnection();
-            try (PreparedStatement ps = conn.prepareStatement("INSERT INTO `" + TABLE_BOX + "`(`uuid`,`sender`,`data`,`time`) VALUES(?, ?, ?, NOW());")) {
-                ps.setString(1, mail.uuid);
-                ps.setString(2, mail.sender);
-                byte[] bytes = mail.serialize().getBytes(StandardCharsets.UTF_8);
-                try (InputStream in = new ByteArrayInputStream(bytes)) {
-                    ps.setBinaryStream(3, in);
-                    ps.execute();
-                }
-            }
-            try (PreparedStatement ps = conn.prepareStatement("INSERT INTO `" + TABLE_STATUS + "`(`uuid`,`receiver`,`read`,`used`) VALUES(?, ?, 0, 0);")) {
-                for (String receiver : mail.receivers) {
-                    ps.setString(1, mail.uuid);
-                    ps.setString(2, receiver);
-                    ps.addBatch();
-                }
-                ps.executeBatch();
-            }
-            mail.noticeSent();
-        } catch (SQLException | IOException e) {
-            SweetMail.warn(e);
-        }
-    }
-
-    @Override
-    public List<MailWithStatus> getOutBox(String player, int page, int perPage) {
-        List<MailWithStatus> mailList = new ArrayList<>();
-        try {
-            int offset = (page - 1) * perPage;
-            Connection conn = dataSource.getConnection();
-            PreparedStatement ps = conn.prepareStatement("SELECT * FROM " +
-                    "(`" + TABLE_STATUS + "` NATURAL JOIN (SELECT * FROM `" + TABLE_BOX + "` WHERE `sender` = ?) as A) " +
-                    "LIMIT " + offset + ", " + perPage + " " +
-                    "ORDER BY `time` DESC;");
-            ps.setString(1, player);
-            try (ResultSet result = ps.executeQuery()) {
-                while (result.next()) {
-                    mailList.add(resolveResult(result));
-                }
-            }
-        } catch (SQLException | IOException e) {
-            SweetMail.warn(e);
-        }
-        return mailList;
-    }
-
-    @Override
-    public List<MailWithStatus> getInBox(boolean unread, String player, int page, int perPage) {
-        List<MailWithStatus> mailList = new ArrayList<>();
-        try {
-            int offset = (page - 1) * perPage;
-            Connection conn = dataSource.getConnection();
-            String conditions = unread
-                    ? "`receiver` = ? AND unread = 1"
-                    : "`receiver` = ?";
-            PreparedStatement ps = conn.prepareStatement("SELECT * FROM " +
-                    "(`" + TABLE_BOX + "` NATURAL JOIN (SELECT * FROM `" + TABLE_STATUS + "` WHERE " + conditions + ") as A) " +
-                    "ORDER BY `used` DESC, `time` DESC " +
-                    "LIMIT " + offset + ", " + perPage + ";");
-            ps.setString(1, player);
-            try (ResultSet result = ps.executeQuery()) {
-                while (result.next()) {
-                    mailList.add(resolveResult(result));
-                }
-            }
-        } catch (SQLException | IOException e) {
-            SweetMail.warn(e);
-        }
-        return mailList;
-    }
-
-    @Override
-    public List<MailWithStatus> getInBoxUnused(String player) {
-        List<MailWithStatus> mailList = new ArrayList<>();
-        try {
-            Connection conn = dataSource.getConnection();
-            PreparedStatement ps = conn.prepareStatement("SELECT * FROM " +
-                    "(`" + TABLE_BOX + "` NATURAL JOIN (SELECT * FROM `" + TABLE_STATUS + "` WHERE `receiver` = ? AND `used` = 1) as A) " +
-                    "ORDER BY `time` DESC;");
-            ps.setString(1, player);
-            try (ResultSet result = ps.executeQuery()) {
-                while (result.next()) {
-                    mailList.add(resolveResult(result));
-                }
-            }
-        } catch (SQLException | IOException e) {
-            SweetMail.warn(e);
-        }
-        return mailList;
-    }
-
-    private MailWithStatus resolveResult(ResultSet result) throws IOException, SQLException {
-        String dataJson;
-        try (InputStream in = result.getBinaryStream("data")) {
-            try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-                byte[] buffer = new byte[1024 * 10];
-                int len;
-                while ((len = in.read(buffer)) != -1) {
-                    out.write(buffer, 0, len);
-                }
-                dataJson = new String(out.toByteArray(), StandardCharsets.UTF_8);
-            }
-        }
-        LocalDateTime time = result.getTimestamp("time").toLocalDateTime();
-        boolean read = result.getInt("read") == 1;
-        boolean used = result.getInt("used") == 1;
-        return Mail.deserialize(dataJson, time, read, used);
-    }
-
-    @Override
-    public void markRead(String uuid, String receiver) {
-        try {
-            Connection conn = dataSource.getConnection();
-            PreparedStatement ps = conn.prepareStatement("UPDATE `" + TABLE_STATUS + "` " +
-                    "SET `read` = 1" +
-                    "WHERE `uuid` = ? AND `receiver` = ?;");
-            ps.setString(1, uuid);
-            ps.setString(2, receiver);
-            ps.execute();
-        } catch (SQLException e) {
-            SweetMail.warn(e);
-        }
-    }
-
-    @Override
-    public void markAllRead(String receiver) {
-        try {
-            Connection conn = dataSource.getConnection();
-            PreparedStatement ps = conn.prepareStatement("UPDATE `" + TABLE_STATUS + "` " +
-                    "SET `read` = 1" +
-                    "WHERE `receiver` = ?;");
-            ps.setString(1, receiver);
-            ps.execute();
-        } catch (SQLException e) {
-            SweetMail.warn(e);
-        }
-    }
-
-    @Override
-    public void markUsed(List<String> uuidList, String receiver) {
-        try {
-            Connection conn = dataSource.getConnection();
-            try (PreparedStatement ps = conn.prepareStatement("UPDATE `" + TABLE_STATUS + "` " +
-                    "SET `used` = 1" +
-                    "WHERE `uuid` = ? AND `receiver` = ?;")) {
-                for (String uuid : uuidList) {
-                    ps.setString(1, uuid);
-                    ps.setString(2, receiver);
-                    ps.addBatch();
-                }
-                ps.executeBatch();
-            }
-        } catch (SQLException e) {
-            SweetMail.warn(e);
-        }
+    protected Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
     }
 
     @Override
