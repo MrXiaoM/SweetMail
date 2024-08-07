48d38334a0996e6c055d195626e6d2a5145d6305
人間工作 <coolxiaom95@gmail.com>
2024-08-07 16:52:45 +0800

    fix style not apply to translate block

--- paper/src/main/java/top/mrxiaom/sweetmail/utils/ItemStackPaper.java
+++ paper/src/main/java/top/mrxiaom/sweetmail/utils/ItemStackPaper.java
@@ -3,6 +3,7 @@ package top.mrxiaom.sweetmail.utils;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.TranslatableComponent;
+import net.kyori.adventure.text.format.Style;
 import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.inventory.ItemStack;
@@ -10,6 +11,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Pattern;
 
 
@@ -18,12 +20,21 @@ public class ItemStackPaper {
     
     static Component parse(String s) {
         TextComponent.Builder builder = Component.text();
+
+        AtomicReference<Style> lastStyle = new AtomicReference<>(null);
         StringHelper.split(translatePattern, ColorHelper.parseColor(s), regexResult -> {
             if (!regexResult.isMatched) {
-                builder.append(LegacyComponentSerializer.legacySection().deserialize(regexResult.text));
+                TextComponent component = LegacyComponentSerializer.legacySection().deserialize(regexResult.text);
+                List<Component> children = component.children();
+                if (!children.isEmpty()) {
+                    Component child = children.get(children.size() - 1);
+                    lastStyle.set(child.style());
+                }
+                builder.append(component);
             } else {
+                Style style = lastStyle.get();
                 TranslatableComponent translatable = Component.translatable(regexResult.result.group(1));
-                builder.append(translatable);
+                builder.append(style == null ? translatable : translatable.style(style));
             }
         });
         return builder.decoration(TextDecoration.ITALIC, false).asComponent();
