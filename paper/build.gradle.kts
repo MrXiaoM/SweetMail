4fe9112bc02deb47c4c2f5b436660711007094e7
MrXiaoM <coolxiaom95@gmail.com>
2025-06-25 09:27:00 +0800

    upgrade dependencies

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 @Suppress("VulnerableLibrariesLocal")
 dependencies {
     compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
-    compileOnly("net.kyori:adventure-api:4.17.0")
-    compileOnly("net.kyori:adventure-platform-bukkit:4.3.4")
-    compileOnly("net.kyori:adventure-text-minimessage:4.17.0")
+    compileOnly("net.kyori:adventure-api:4.22.0")
+    compileOnly("net.kyori:adventure-platform-bukkit:4.4.0")
+    compileOnly("net.kyori:adventure-text-minimessage:4.22.0")
 }
