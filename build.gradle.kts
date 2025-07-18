b3498715dcc182b3caf18bad5c155d5f49e2e595
MrXiaoM <coolxiaom95@gmail.com>
2025-07-18 11:02:09 +0800

    support 1.21.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,7 +76,7 @@ dependencies {
     implementation("net.kyori:adventure-platform-bukkit:4.4.0")
     implementation("net.kyori:adventure-text-serializer-gson:4.22.0")
     implementation("net.kyori:adventure-text-minimessage:4.22.0")
-    implementation("de.tr7zw:item-nbt-api:2.15.1")
+    implementation("de.tr7zw:item-nbt-api:2.15.2-SNAPSHOT")
     implementation("com.github.technicallycoded:FoliaLib:0.4.4")
     implementation(project(":paper"))
     "shadowLink"(project(":paper:craft-engine"))
