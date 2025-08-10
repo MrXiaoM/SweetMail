2b76a528873a24d28b9edc91ac27a250022243d6
MrXiaoM <coolxiaom95@gmail.com>
2025-08-10 21:10:53 +0800

    upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,7 +87,7 @@ dependencies {
     // Shadow Dependency
     implementation("de.tr7zw:item-nbt-api:2.15.2-SNAPSHOT")
     implementation("com.github.technicallycoded:FoliaLib:0.4.4") { isTransitive = false }
-    implementation("top.mrxiaom:LibrariesResolver:1.5.7:all") { isTransitive = false }
+    implementation("top.mrxiaom:LibrariesResolver:1.5.8:all")
     implementation(project(":paper"))
     "shadowLink"(project(":paper:craft-engine"))
 }
